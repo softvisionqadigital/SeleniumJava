ab50399988fa53789fa3b2f4f910a5d4a2b68dc0
RaghunandanM <raghunandan.m@softvision.com>
2020-05-27 13:22:42 +0530

    Update test1.java
    updated from github desktop

--- eclipse/learning/src/learning/test1.java
+++ eclipse/learning/src/learning/test1.java
@@ -9,6 +9,10 @@ public class test1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Github Code Push");
+		System.out.println("Github Code pulls");
+		System.out.println("Github Code pulls");
+		
+		//git@github.com:softvisionqadigital/SeleniumJava.git
 	}
 
 }
