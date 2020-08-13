21d07a795bca85418231030072d42643753b5759
RaghunandanM <65584386+RaghunandanM@users.noreply.github.com>
2020-08-13 12:53:48 +0530

    Update test1.java
    updated

--- eclipse/learning/src/learning/test1.java
+++ eclipse/learning/src/learning/test1.java
@@ -1,15 +1,9 @@
 package learning;
-
-
-
-
-
 public class test1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Github Code Push and Pull");
-		
+		System.out.println("Github Code Push and Pull");	
 		
 	}
 
