17aba3d90a43dfa0babf001e129b6b1fc332530a
Shivumm860763 <shivakumar.mm@softvision.com>
2020-09-23 13:45:06 +0530

    TestNG with maven
    TestNG with maven

---
+++ E2ETesting/src/test/java/E2EFramework/ValidateTitle.java
@@ -0,0 +1,50 @@
+package E2EFramework;
+import java.io.IOException;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pageObjects.LandingPage;
+import pageObjects.LoginPage;
+import resources.Base;
+
+public class ValidateTitle extends Base {
+
+		public WebDriver driver;
+		public static Logger log =LogManager.getLogger(Base.class.getName());
+		@BeforeTest
+		public void initialize() throws IOException
+		{
+			driver =initializeDriver();
+			 log.info("Driver is initialized");
+				
+			driver.get(prop.getProperty("url"));
+			 log.info("Navigated to Home page");
+		}
+		@Test
+		
+		public void validateAppTitle() throws IOException
+		{
+			
+			LandingPage l=new LandingPage(driver);
+			Assert.assertEquals(l.getTitle().getText(), "FEATURED COU123RSES");
+			System.out.println("Test completed");
+		}
+		@AfterTest
+		public void teardown()
+		{
+			driver.close();
+			
+		}
+
+
+}
---
+++ E2ETesting/target/classes/ExtentReporterNG.java
@@ -0,0 +1,23 @@
+package resources;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+	static ExtentReports extent;
+	
+	public static ExtentReports getReportObject()
+	{
+		
+		String path =System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("E2E Framework Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		 extent =new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Shivakumar M M");
+		return extent;
+		
+	}
+}
