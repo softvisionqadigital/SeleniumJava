422aa82bb1af9505953eab88c5b47b460badab29
Thejaswi1691 <thejaswi.k@softvision.com>
2020-10-06 19:45:12 +0530

    selenium

--- BasicsJava/src/SeleniumPractice/Dropdowns.java
+++ BasicsJava/src/SeleniumPractice/Dropdowns.java
@@ -1,7 +1,12 @@
 package SeleniumPractice;
 
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Dropdowns {
 
@@ -9,9 +14,29 @@ public class Dropdowns {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		driver.close();
+		driver.get("https://chercher.tech/practice/practice-dropdowns-selenium-webdriver");
+		WebElement drpselect=driver.findElement(By.cssSelector("select[id=first]"));
+		Select drpdn=new Select(drpselect);
 		
+		System.out.println("hey theju! ,Enter on which basis u wanted the element to be selected: value or  index or visibletext");
+		Scanner input=new Scanner(System.in);
+		String userinput=input.nextLine();
+		if(userinput.equals("index"))
+		{	
+		System.out.println("element selected by using index");
+		drpdn.selectByIndex(2);
+		}
+		if(userinput.equals("value"))
+		{
+			System.out.println("element selected by using value");
+			drpdn.selectByValue("Google");
+		}
+		if(userinput.equals("visibletext"))
+		{
+			System.out.println("element selected by using visible text");
+			drpdn.selectByVisibleText("Yahoo");
+		}
+
 		
 	}
 
